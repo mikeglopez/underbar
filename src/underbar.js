5a8309241fb8c41e17c49159a6d3d1957f654da7
Mike Lopez <mikeglopez@me.com>
2018-10-10 11:21:53 -0500

    "Implement 'last' function"

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,10 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length - 1] : array.slice(array[n]);
+    if (n === 0) {
+      return [];
+    }
+    return n === undefined ? array[array.length - 1] : array.slice(-n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
