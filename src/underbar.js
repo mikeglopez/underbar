3cc5fa94de3bbfc6aa2271b9f0b788953bf499cc
Mike Lopez <mikeglopez@me.com>
2018-10-14 20:53:40 -0500

    "Implement extend function"

--- src/underbar.js
+++ src/underbar.js
@@ -258,6 +258,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 0; i < arguments.length; i++) { // for each object passed in
+      for (var key in arguments[i]) { // for each key in the object
+          obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
